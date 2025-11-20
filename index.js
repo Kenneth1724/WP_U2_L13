dedd51c74d8583e615be3c5081a177c70cb39b0e
Max <maxsnyder12345@gmail.com>
2025-11-20 14:26:23 -0500

    edited JS

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 let gameOver = false;
 let turn = 1;
-
-
-function card_gen(){
-const card = ["../../resources/bear.png", "../../resources/bear.png", "../../resources/bear.png",
-     "../../resources/bear.png", "../../resources/bear.png", "../../resources/bear.png",
-      "../../resources/bear.png", "../../resources/bear.png", "../../resources/bear.png",
-    "../../resources/bear.png",
+const card = ["resources/bear.png", "resources/football.png", "resources/bear.png",
+     "resources/bear.png", "resources/bear.png", "resources/bear.png",
+      "resources/bear.png", "resources/bear.png", "resources/bear.png",
+    "resources/bear.png",
 ]
 
 
 
-}
-
-
-function user_clicks(div){
-
+function user_clicks(){
     const pic = document.getElementsByClassName("inside_div")
+    const picClicked = pic[0].id.replace("row","")
+    console.log(picClicked)
     pic[0].style.background = `url(${card[0]})`
 }

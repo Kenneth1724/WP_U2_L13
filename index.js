c094426163f7ddb1c6a22fb48af2962eea259a94
Max <maxsnyder12345@gmail.com>
2025-11-21 12:30:29 -0500

    trying to make onclick work for cells

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 let gameOver = false;
 let turn = 1;
+let currentPic = 0;
 const card = ["resources/bear.png", "resources/football.png", "resources/bear.png",
      "resources/bear.png", "resources/bear.png", "resources/bear.png",
       "resources/bear.png", "resources/bear.png", "resources/bear.png",
     "resources/bear.png",
 ]
+const pic = document.getElementsByClassName("inside_div")
 
 
 
 function user_clicks(){
-    const pic = document.getElementsByClassName("inside_div")
-    pic.onclick = 
-    console.log(picClicked)
-    pic.style.background = `url(${card[0]})`
+    
+    while(currentPic!=currentPic.length) {
+        console.log(pic[0])
+        pic[currentPic].onclick = function() {
+            alert("test")
+        }
+        currentPic++
+    }
+    //pic.style.background = `url(${card[0]})`
     
 }

72bfb66d617d3c148ee96c7ced25252e50cb8ef3
Max <maxsnyder12345@gmail.com>
2025-11-21 14:15:41 -0500

    attempting to make image clicks work

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 let gameOver = false;
 let turn = 1;
 let currentPic = 0;
-const card = ["resources/bear.png", "resources/football.png", "resources/bear.png",
-     "resources/bear.png", "resources/bear.png", "resources/bear.png",
-      "resources/bear.png", "resources/bear.png", "resources/bear.png",
-    "resources/bear.png",
+const card = ["resources/bear.png", "resources/football.png", "resources/leafs.png",
+     "resources/mouse.png", "resources/pie.png", "resources/pumpkin.png",
+      "resources/scarecrow.png", "resources/soccer.png", "resources/turkey.png",
+    "resources/wind.png",
 ]
+let usedIndex = [];
 const pic = document.getElementsByClassName("inside_div")
 
 
 
 function user_clicks(){
-    let rIndex = Math.floor(Math.random() * 10) + 1;
-    //console.log(rIndex)
     while(currentPic!=20) {
         pic[currentPic].onclick = function() {
-            this.style.background=`url(${card[0]})`
+            let rIndex = Math.floor(Math.random() * 9) + 1;
+            if(usedIndex.length!=9) {    
+                if(usedIndex.includes(rIndex)) {
+                    rIndex = Math.floor(Math.random() * 9) + 1;
+                } else {
+                    usedIndex.push(rIndex)
+                }
+            } else {
+                usedIndex=[]
+            }
+            //console.log(rIndex)
+            console.log(usedIndex)
+            console.log(usedIndex.length)
+            this.style.background=`url(${card[rIndex]}) no-repeat center`
+            this.style.backgroundSize="cover";
         }
         currentPic++
     }

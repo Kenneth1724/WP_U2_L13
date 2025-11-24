e9979c7e77d313cd38e4a33e26b4c3f905f13433
Max <maxsnyder12345@gmail.com>
2025-11-24 12:07:30 -0500

    added comments to js code for readability

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let gameOver = false;
 let turn = 1;
 let currentPic = 0;
+let idk = 0;
 const card = ["resources/bear.png", "resources/football.png", "resources/leafs.png",
      "resources/mouse.png", "resources/pie.png", "resources/pumpkin.png",
       "resources/scarecrow.png", "resources/soccer.png", "resources/turkey.png",
@@ -14,21 +15,34 @@ const pic = document.getElementsByClassName("inside_div")
 function user_clicks(){
     while(currentPic!=20) {
         pic[currentPic].onclick = function() {
-            let rIndex = Math.floor(Math.random() * 9) + 1;
-            if(usedIndex.length!=9) {    
-                if(usedIndex.includes(rIndex)) {
-                    rIndex = Math.floor(Math.random() * 9) + 1;
-                } else {
-                    usedIndex.push(rIndex)
+            idk++
+            // Chooses a random index
+            let rIndex = Math.floor(Math.random() * 10) + 0;
+            
+            // Chooses a random index again if the index is already used
+            if(usedIndex.length!=10) {    
+                while(usedIndex.includes(rIndex)) {
+                    rIndex = Math.floor(Math.random() * 10) + 0;
                 }
             } else {
                 usedIndex=[]
             }
-            //console.log(rIndex)
-            console.log(usedIndex)
-            console.log(usedIndex.length)
+           
+            // Adds the index used to a list of used indexes
+            usedIndex.push(rIndex)
+            
+            // Changing from black background to image when clicked
             this.style.background=`url(${card[rIndex]}) no-repeat center`
             this.style.backgroundSize="cover";
+
+            // Resetting the cards after two cards are selected
+            if(idk==3) {
+                for(let i=0;i<pic.length;i++) {
+                    pic[i].style.backgroundImage="none";
+                    pic[i].style.backgroundColor="black";
+                }
+                idk=0
+            }
         }
         currentPic++
     }
@@ -36,6 +50,6 @@ function user_clicks(){
     
 }
 
-function assignImages(pic, rIndex) {
+/*function assignImages(pic, rIndex) {
     cards = getRandomElement(card)
-}
+}*/
